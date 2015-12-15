901a4fbdb5f9a50def39a0cba20e641657472d74
Galya Koleva <Galya Koleva>
2015-12-15 14:00:38 +0200

    Javascript homework

---
+++ lesson7/js/main1.js
@@ -0,0 +1,11 @@
+function myFunct (parentNode, childObj) {
+	var x = childObj.parentNode;
+    x.style.backgroundColor = "red";
+}
+
+function myBlur (parentNode, childObj) {
+	var y = childObj.parentNode;
+    y.style.backgroundColor = "#777777";
+}
+
+
